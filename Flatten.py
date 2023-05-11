91ee0a7daed1ed0dfdeeb47d0fc3dc6abd9ff693
Genos-Noctua <106928018+Genos-Noctua@users.noreply.github.com>
2023-05-11 23:36:50 +0300

    Update Flatten.py
    Ignore that, it's fine

--- Flatten.py
+++ Flatten.py
@@ -1,16 +1,24 @@
-import glob
-import os
+from os import path, listdir, mkdir, rename
 import tkinter.filedialog
+from os.path import isfile, join, isdir
 
 music_dir = tkinter.filedialog.askdirectory()
-files = sorted(
-    [int(os.path.splitext(os.path.basename(f))[0]) for f in glob.glob(os.path.join(music_dir, "*.mp3")) if os.path.isfile(f)]
-)
-holes = [x for x in range(1, len(files)) if x not in files] + [len(files)]
-
-while len(holes) > 0:
-    src = os.path.join(music_dir, f"{files.pop()}.mp3")
-    dst = os.path.join(music_dir, f"{holes.pop()}.mp3")
-    os.rename(src, dst)
-
-print("Done")
+files = [f for f in listdir(music_dir) if isfile(join(music_dir, f))]
+length = len(files)
+for x in range(length):
+    try:
+        files[x] = int(files[x][:-4])
+    except ValueError:
+        files.pop(x)
+files = sorted(files)
+holes = list()
+for x in range(1, length):
+    if not isfile(join(music_dir, str(str(x) + ".mp3"))):
+        holes.append(x)
+for x in range(len(holes)):
+    rename(
+        str(music_dir) + "/" + str(files[len(files) - 1]) + ".mp3",
+        str(music_dir) + "/" + str(holes[len(holes) - 1]) + ".mp3",
+    )
+    files.pop(len(files) - 1)
+    holes.pop(len(holes) - 1)
