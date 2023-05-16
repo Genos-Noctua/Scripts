ec6409cb7c61bb7a91a417fd166d3907f0f0b317
Genos-Noctua <106928018+Genos-Noctua@users.noreply.github.com>
2023-05-16 14:56:34 +0300

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -2,31 +2,27 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 from PIL import Image
 import numpy as np
+from tqdm import tqdm
 
 root = Tk()
 root.withdraw()
-
 files = askopenfilenames()
 
-arr_list = []
 max_size = (0, 0)
+with Image.open(files[1]) as img:
+    arr = np.array(img)
+    typee = arr.dtype
+    max_size = img.size
 
-for file in files:
-    with Image.open(file) as img:
-        arr = np.array(img)
-        arr_list.append(arr)
-        if img.size > max_size:
-            max_size = img.size
+means = np.zeros(max_size[::-1] + (3,), dtype=np.uint64)
 
-means = np.zeros(max_size[::-1] + (3,), dtype=np.float32)
+for fil in tqdm(files): 
+    with Image.open(fil) as img: 
+        arr = np.array(img).astype(np.uint64) 
+        means += arr
 
-for arr in arr_list:
-    img_arr = Image.fromarray(arr)
-    img_arr = img_arr.resize(max_size, resample=Image.Resampling.BICUBIC)
-    arr = np.array(img_arr)
-    means += arr.astype(np.float32)
-
-means /= len(arr_list)
+means = means.astype(np.float64)
+means /= len(files)
 means = means.astype(np.uint8)
 Image.fromarray(means).save("output.png")
 
