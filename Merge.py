69f5be4e88d2dd1fdcb856324036adde8b5f9938
Genos-Noctua <106928018+Genos-Noctua@users.noreply.github.com>
2023-05-24 13:29:02 +0300

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -1,20 +1,13 @@
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
-from tkinter import Tk
 from os.path import dirname
 from tkinter.filedialog import askopenfilenames
 
-root = Tk()
-root.withdraw()
 files = askopenfilenames()
 
 max_size = (0, 0)
-with Image.open(files[1]) as img:
-    arr = np.array(img)
-    typee = arr.dtype
-    max_size = img.size
-
+with Image.open(files[0]) as img: max_size = img.size
 means = np.zeros(max_size[::-1] + (3,), dtype=np.uint64)
 
 for fil in tqdm(files): 
@@ -27,6 +20,3 @@ means /= len(files)
 means = means.astype(np.uint8)
 output_dir = dirname(files[0])
 Image.fromarray(means).save(output_dir + '/output.png')
-
-root.quit()
-root.destroy()
