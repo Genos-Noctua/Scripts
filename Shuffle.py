f056b143b19266bbfd0d7ff6e80fb75562821eb8
Genos-Noctua <106928018+Genos-Noctua@users.noreply.github.com>
2023-05-28 18:54:23 +0300

    Update Shuffle.py

--- Shuffle.py
+++ Shuffle.py
@@ -1,25 +1,18 @@
-import os, shutil, tkinter.filedialog, random, string
+import os, shutil, tkinter.filedialog, random, string, tqdm
 
 src_dir = tkinter.filedialog.askdirectory()
-
 temp_dir = os.path.join(src_dir, 'temp')
 os.makedirs(temp_dir, exist_ok=True)
 
-for file in os.listdir(src_dir):
+for file in tqdm.tqdm(os.listdir(src_dir), smoothing=3, desc='Перенос и перемешивание'):
     if '.mp3' in file:
         src_file = os.path.join(src_dir, file)
-        dst_file = os.path.join(temp_dir, file)
+        dst_file = os.path.join(temp_dir, os.path.join(temp_dir, ''.join(random.choices(string.ascii_lowercase, k=50)) + os.path.splitext(file)[1]))
         shutil.move(src_file, dst_file)
 
 files = [f for f in os.listdir(temp_dir) if os.path.isfile(os.path.join(temp_dir, f))]
-for file in files:
-    src_file = os.path.join(temp_dir, file)
-    rnd_file = os.path.join(temp_dir, ''.join(random.choices(string.ascii_lowercase, k=8)) + os.path.splitext(file)[1])
-    os.rename(src_file, rnd_file)
-    
-files = [f for f in os.listdir(temp_dir) if os.path.isfile(os.path.join(temp_dir, f))]
 x = 1
-for file in files:
+for file in tqdm.tqdm(files, smoothing=3, desc='Возврат'):
     src_file = os.path.join(temp_dir, file)
     dst_file = os.path.join(src_dir, str(x)+'.mp3')
     os.rename(src_file, dst_file)
