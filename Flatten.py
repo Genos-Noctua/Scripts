45721888c01bbdd251c49fda3c5bd96a6aba580c
Genos-Noctua <106928018+Genos-Noctua@users.noreply.github.com>
2023-06-17 23:41:22 +0300

    Update Flatten.py

--- Flatten.py
+++ Flatten.py
@@ -1,6 +1,6 @@
-from os import path, listdir, mkdir, rename
+from os import listdir, rename
 import tkinter.filedialog, tqdm
-from os.path import isfile, join, isdir
+from os.path import isfile, join
 
 music_dir = tkinter.filedialog.askdirectory()
 files = [f for f in listdir(music_dir) if isfile(join(music_dir, f))]
@@ -17,8 +17,6 @@ for x in range(1, length):
         holes.append(x)
 for x in tqdm.tqdm(range(len(holes)), smoothing=0):
     rename(
-        str(music_dir) + "/" + str(files[len(files) - 1]) + ".mp3",
-        str(music_dir) + "/" + str(holes[len(holes) - 1]) + ".mp3",
+        str(music_dir) + "/" + str(files.pop()) + ".mp3",
+        str(music_dir) + "/" + str(holes.pop()) + ".mp3",
     )
-    files.pop(len(files) - 1)
-    holes.pop(len(holes) - 1)
