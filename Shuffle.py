ad9a7b4e71f111338a761997cb4c15da582fc0cb
GENOS <Genos.noctua@gmail.com>
2023-06-23 01:51:49 +0300

    Update Shuffle.py

--- Shuffle.py
+++ Shuffle.py
@@ -1,10 +1,10 @@
-import os, shutil, tkinter.filedialog, random, string, tqdm
-
+import os, shutil, tkinter.filedialog, random, string
+from tqdm import tqdm
 src_dir = tkinter.filedialog.askdirectory()
 temp_dir = os.path.join(src_dir, 'temp')
 os.makedirs(temp_dir, exist_ok=True)
 
-for file in tqdm.tqdm(os.listdir(src_dir), smoothing=0, desc='Перенос и перемешивание'):
+for file in tqdm(os.listdir(src_dir), smoothing=0, desc='Перенос и перемешивание'):
     if '.mp3' in file:
         src_file = os.path.join(src_dir, file)
         dst_file = os.path.join(temp_dir, os.path.join(temp_dir, ''.join(random.choices(string.digits, k=8)) + os.path.splitext(file)[1]))
@@ -12,7 +12,7 @@ for file in tqdm.tqdm(os.listdir(src_dir), smoothing=0, desc='Перенос и 
 
 files = [f for f in os.listdir(temp_dir) if os.path.isfile(os.path.join(temp_dir, f))]
 x = 1
-for file in tqdm.tqdm(files, smoothing=0, desc='Возврат'):
+for file in tqdm(files, smoothing=0, desc='Возврат'):
     src_file = os.path.join(temp_dir, file)
     dst_file = os.path.join(src_dir, str(x)+'.mp3')
     os.rename(src_file, dst_file)
